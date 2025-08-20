f84f8d08b1edd31204eaa932e3b5ca1c7d522318
hanseulhee <3021062@gmail.com>
2025-08-20 18:01:01 +0900

    가입 관련 유효성 검사 상수 정의

---
+++ src/constants/validationMessages.ts
@@ -0,0 +1,21 @@
+/**
+* @file validationMessages.ts
+* @description
+* 유효성 검사에 사용되는 정규식(regex)과
+* 사용자에게 표시되는 에러 메시지를 상수로 정의한 파일입니다.
+*/
+
+// email
+export const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+export const ERROR_EMAIL_CHECK = '이메일 형식을 확인해주세요.';
+
+// password
+export const ERROR_PASSWORD_CHECK = '비밀번호는 영문, 숫자, 특수문자만 사용할 수 있습니다.'
+export const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+export const ERROR_PASSWORD_MIN = '비밀번호는 최소 8자 이상이어야 합니다.'
+export const ERROR_PASSWORD_MAX = '비밀번호는 최대 20자까지 입력할 수 있습니다.'
+export const ERROR_PASSWORD_AGAIN = '비밀번호를 한 번 더 입력해 주세요.'
+export const ERROR_PASSWORD_DIFFERENT = '현재 비밀번호와 다른 새 비밀번호를 입력해 주세요.'
+
+// login
+export const ERROR_LOGIN = '이메일 또는 비밀번호를 확인해 주세요.'
